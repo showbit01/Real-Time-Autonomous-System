6ac4c3dc1dabac6f07e0cf71eefdb86643c7910d
Shobhit Sharma <68598980+sh0wbit01@users.noreply.github.com>
2021-01-18 19:52:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+
+
+import threading
+import time
+import random
+import sys
+import multiprocessing as mp
+import os
+
+# import agent types (positions)
+from AI.soccerpy.agent import Agent as A0
+# strikers
+from AI.s import Agent as A1
+# defenders
+from AI.d import Agent as A2
+# goalie
+from AI.g import Agent as A3
+
+# set team
+TEAM_NAME = 'shobhit'
+NUM_PLAYERS = 11
+
+
+if __name__ == "__main__":
+
+    # return type of agent: midfield, striker etc.
+    def agent_type(position):
+    	return {
+            2: A2,
+            3: A3,
+            4: A2,
+            6: A2,
+            7: A2,
+    		8: A2,
+    	}.get(position, A1)
+
+    # spawn an agent of team_name, with position
+    def spawn_agent(team_name, position):
+        """
+        Used to run an agent in a seperate physical process.
+        """
+        # return type of agent by position, construct
+        a = agent_type(position)()
+        a.connect("localhost", 6000, team_name)
+        a.play()
+
+        # we wait until we're killed
+        while 1:
+            # we sleep for a good while since we can only exit if terminated.
+            time.sleep(1)
+
+    # spawn all agents as seperate processes for maximum processing efficiency
+    agentthreads = []
+    for position in xrange(1, NUM_PLAYERS+1):
+        print "  Spawning agent %d..." % position
+
+        at = mp.Process(target=spawn_agent, args=(TEAM_NAME, position))
+        at.daemon = True
+        at.start()
+
+        agentthreads.append(at)
+
+    print "Spawned %d agents." % len(agentthreads)
+    print
+    print "Playing soccer..."
+
+    # wait until killed to terminate agent processes
+    try:
+        while 1:
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        print
+        print "Killing agent threads..."
+
+        # terminate all agent processes
+        count = 0
+        for at in agentthreads:
+            print "  Terminating agent %d..." % count
+            at.terminate()
+            count += 1
+        print "Killed %d agent threads." % (count - 1)
+
+        print
+        print "Exiting."
+        sys.exit()
+
